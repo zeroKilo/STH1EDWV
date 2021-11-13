ece199ccdd540e958cc3f8904505cccf41541aed
Maxim <maxim@smspower.org>
2021-11-13 16:32:00 +0000

    Block tile picker was broken

--- editor/TileChooser.cs
+++ editor/TileChooser.cs
@@ -4,17 +4,25 @@ namespace sth1edwv
 {
     public partial class TileChooser : Form
     {
+        private readonly bool _inConstructor;
+
         public TileChooser(TileSet tileSet, int selectedIndex, Palette palette)
         {
             InitializeComponent();
             tilePicker1.SetData(tileSet.Tiles, palette);
+            _inConstructor = true;
             tilePicker1.SelectedIndex = selectedIndex;
+            _inConstructor = false;
         }
 
         public Tile SelectedTile => tilePicker1.SelectedItem as Tile;
 
         private void tilePicker1_SelectionChanged(object sender, IDrawableBlock e)
         {
+            if (_inConstructor)
+            {
+                return;
+            }
             DialogResult = e == null ? DialogResult.Cancel : DialogResult.OK;
             Close();
         }
